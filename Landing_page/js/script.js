f226970650419564ed7e873883307115c09d8e94
user <levkovskaya.masha@gmail.com>
2018-08-15 22:45:26 +0300

    Landing

--- Landing_page/js/script.js
+++ Landing_page/js/script.js
@@ -1,9 +1,14 @@
+$(function () {
+    const scroll = $(document.documentElement);
 
-const scroll = $(document.documentElement);
-
-$(".scroll").click(function () {
-    const scroll_to = $(this).attr("href");
-    const top = $(scroll_to).offset().top;
-        alert($(this).attr("href"))
+    $(".scroll").on('click', function () {
+        const scroll_to = $(this).attr("href");
+        const top = $(scroll_to).offset().top;
+        scroll.animate({
+            scrollTop: top
+        }, 1000);
+    }
+    );
 });
+
 
