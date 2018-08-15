fe8b39b10ceb575b4a9b02e006eba29355c8fa23
user <levkovskaya.masha@gmail.com>
2018-08-15 23:54:43 +0300

    Landing

--- Landing_page/js/script.js
+++ Landing_page/js/script.js
@@ -1,14 +1,35 @@
 $(function () {
     const scroll = $(document.documentElement);
+    const height = $("#intro").innerHeight() - 1;
 
     $(".scroll").on('click', function () {
         const scroll_to = $(this).attr("href");
         const top = $(scroll_to).offset().top;
         scroll.animate({
             scrollTop: top
-        }, 1000);
-        return false;
-    } )
-};
-
-
+        }, 1000, function () {
+            if (height < scroll.scrollTop()) {
+                $("#top").addClass("visible");
+            }
+            else {
+                $("#top").removeClass("visible");
+            }
+        };
+    };
+    $(window).on('scroll', function () {
+        if (height < scroll.scrollTop()) {
+            $("#top").addClass("visible");
+        }
+        else {
+            $("#top").removeClass("visible");
+        }
+    };
+    $(document).on('ready', function () {
+        if (height < scroll.scrollTop()) {
+            $("#top").addClass("visible");
+        }
+        else {
+            $("#top").removeClass("visible");
+        }
+    };
+});
